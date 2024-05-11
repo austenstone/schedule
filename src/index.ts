3035ab53631e3d9f11d221d4adca98e44d3af032
Austen Stone <austenstone@github.com>
2024-05-11 09:55:08 -0400

    chore: Update npm dependencies and workflows

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,19 @@
-import { getInput, info } from "@actions/core";
-import { getOctokit } from "@actions/github";
-import { getProjects } from "./projects";
+import { getInput, setOutput } from "@actions/core";
+import dayjs from 'dayjs'
 
 interface Input {
-  token: string;
-  projectName: string;
-  owner: string;
-  repo: string;
+  date: string;
 }
 
 const getInputs = (): Input => {
   const result = {} as Input;
-  result.token = getInput("github-token");
-  result.projectName = getInput("project-name");
-  result.owner = getInput("owner");
-  result.repo = getInput("repo");
-  if (result.repo.includes("/")) {
-    const parts = result.repo.split("/");
-    result.repo = parts[1];
-  }
-  if (!result.token || result.token === "") {
-    throw new Error("github-token is required");
-  }
+  result.date = getInput("date");
   return result;
 }
 
 export const run = async (): Promise<void> => {
-  const input = getInputs();
-  const octokit = getOctokit(input.token);
-  const ownerRepo = {
-    owner: input.owner,
-    repo: input.repo,
-  };
-
-  try {
-    info(`Getting projects for ${input.owner}/${input.repo}`)
-    const projects = await getProjects(octokit, ownerRepo.owner, ownerRepo.repo, input.projectName);
-    console.log(JSON.stringify(projects, null, 2));
-  } catch (error) {
-    if (error instanceof Error) {
-      info(`Error: ${error.message}`);
-    }
-  }
+  const inputs = getInputs();
+  setOutput('result', dayjs().isAfter(dayjs(inputs.date)));
 };
 
 run();
