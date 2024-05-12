1ab05a26d6816967b20fc19872121bb55d80fcb8
Austen Stone <austenstone@github.com>
2024-05-11 22:39:10 -0400

    test: add try-catch block to handle errors in run function

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,10 +3,11 @@ import * as main from '../src/index'
 
 const runMock = jest.spyOn(main, 'run').mockImplementation()
 
-test('test run', () => {
-  it('calls run when imported', async () => {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    import('../src/index')
-    expect(runMock).toHaveBeenCalled()
-  })
+test('run', () => {
+  try {
+    main.run()
+  } catch (error) {
+    console.error(error)
+  }
+  expect(runMock).toHaveBeenCalled()
 });
